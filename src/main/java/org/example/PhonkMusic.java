11510a3eda5fdbc3ec00059f3d6e3a3bc6907d02
Dmytro Horodyskyi <107617154+Dmytro1177@users.noreply.github.com>
2022-10-02 22:17:29 +0300

    v1
    v1

---
+++ src/main/java/org/example/PhonkMusic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class PhonkMusic implements Music{
+    @Override
+    public String getSong(){
+        return "PakMane";
+    }
+}
---
+++ src/main/java/org/example/RockMusicc.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class RockMusicc implements Music{
+    @Override
+    public String getSong(){
+        return "Wind cries Mary";
+    }
+
+}
