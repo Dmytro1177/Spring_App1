c143a59e76551c3bee49fc6ad014265ef313edf7
Dmytro1177 <dimagorodiskij@gmail.com>
2022-10-07 17:10:01 +0300

    v3

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     private String name;
     private int volume;
@@ -21,14 +27,16 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music){
-        this.music = music;
+
+    public MusicPlayer(List<Music> musicList){
+        this.musicList = musicList;
     }
     public MusicPlayer(){}
-    public void setMusic(Music music){
-        this.music=music;
-    }
+    //public void setMusic(Music music){
+    //this.music=music;
+    //}
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for(int i=0; i < musicList.size(); i++)
+        System.out.println("Playing: " + musicList.get(i).getSong());
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -11,7 +11,8 @@ public class TestSpring {
         musicPlayer.playMusic();
 
         System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer.getVolume()
+);
 
         context.close();
     }
