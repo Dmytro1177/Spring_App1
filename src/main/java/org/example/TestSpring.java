ae598ed7313bccb1cd31aef197b8784d21b25b95
Dmytro1177 <dimagorodiskij@gmail.com>
2022-10-07 19:42:38 +0300

    v5

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,7 +6,15 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("classical", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+
+
+
+//        ClassicalMusic classicalMusic1 = new ClassicalMusic();
+
+       /* MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         boolean comparison = firstMusicPlayer == secondMusicPlayer;
@@ -18,7 +26,7 @@ public class TestSpring {
 
         firstMusicPlayer.setVolume(15);
         System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());*/
         //System.out.println(musicPlayer.getName());
         //System.out.println(musicPlayer.getVolume());
 
